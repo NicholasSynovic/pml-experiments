1091121727ce6193a438ff0ca413b4ea250f23b7
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2022-06-08 22:10:28 -0500

    Format code

--- pml_experiments/perceptron/iris.py
+++ pml_experiments/perceptron/iris.py
@@ -1,24 +1,28 @@
-from sklearn import datasets
-from pandas import DataFrame
-from sklearn.utils._bunch import Bunch
-from pandas import Series
 from numpy import ndarray, stack
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
+from pandas import DataFrame, Series
+from sklearn import datasets
 from sklearn.linear_model import Perceptron
 from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.utils._bunch import Bunch
 
-def loadIrisDataset()   ->  DataFrame:
+
+def loadIrisDataset() -> DataFrame:
     iris: Bunch = datasets.load_iris(as_frame=True)
     data: DataFrame = iris.data
     target: Series = iris.target
     data["target"] = target
     return data
 
-def dropTarget(data: DataFrame, target: int) ->  DataFrame:
+
+def dropTarget(data: DataFrame, target: int) -> DataFrame:
     return data[data["target"] != target].reset_index(drop=True)
 
-def splitData(data: DataFrame, attribute1_Index: int, attribut2_Index: int, testSize: float = 0.3)    ->  tuple[ndarray, ndarray, ndarray, ndarray]:
+
+def splitData(
+    data: DataFrame, attribute1_Index: int, attribut2_Index: int, testSize: float = 0.3
+) -> tuple[ndarray, ndarray, ndarray, ndarray]:
     a1: ndarray = data[data.columns[attribute1_Index]].to_numpy()
     a2: ndarray = data[data.columns[attribut2_Index]].to_numpy()
 
@@ -29,11 +33,14 @@ def splitData(data: DataFrame, attribute1_Index: int, attribut2_Index: int, test
     xTest: ndarray
     yTrain: ndarray
     yTest: ndarray
-    xTrain, xTest, yTrain, yTest = train_test_split(x, y, test_size=testSize, random_state=1, stratify=y)
+    xTrain, xTest, yTrain, yTest = train_test_split(
+        x, y, test_size=testSize, random_state=1, stratify=y
+    )
 
     return (xTrain, xTest, yTrain, yTest)
 
-def transformData(xTrain: ndarray, xTest:ndarray)   ->  tuple[ndarray, ndarray]:
+
+def transformData(xTrain: ndarray, xTest: ndarray) -> tuple[ndarray, ndarray]:
     sc: StandardScaler = StandardScaler()
     sc.fit(X=xTrain)
 
@@ -42,9 +49,10 @@ def transformData(xTrain: ndarray, xTest:ndarray)   ->  tuple[ndarray, ndarray]:
 
     return (transformedTrain, transformedTest)
 
-def main()  ->  None:
+
+def main() -> None:
     df: DataFrame = loadIrisDataset()
-    df = dropTarget(data = df, target = 0)
+    df = dropTarget(data=df, target=0)
     dataSets: tuple = splitData(data=df, attribute1_Index=0, attribut2_Index=1)
     transformedData: tuple = transformData(xTrain=dataSets[0], xTest=dataSets[1])
     ppn: Perceptron = Perceptron(eta0=0.1, random_state=1)
@@ -52,6 +60,7 @@ def main()  ->  None:
     ppn: ndarray = ppn.predict(X=transformedData[1])
 
     print(f"Accuracy: {accuracy_score(y_true=dataSets[3], y_pred=ppn) * 100}%")
+
 
 if __name__ == "__main__":
     main()
